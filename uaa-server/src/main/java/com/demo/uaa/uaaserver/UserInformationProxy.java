f0ac8d64b4dabd333629f58a5bc4db82bcb6e0b3
Girish-Mehta <girish.mehta100@gmail.com>
2018-04-11 12:36:16 +0530

    Added files

---
+++ uaa-server/src/main/java/com/demo/uaa/uaaserver/UserInformationProxy.java
@@ -0,0 +1,14 @@
+package com.demo.uaa.uaaserver;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(value="zuul")
+@RibbonClient(name="registration-service")
+public interface UserInformationProxy {
+
+	@PostMapping("registration-service/login")
+	public String getUserInformationn(@RequestBody UserInformation userInfoInBody);
+}
